73ea0c2925da11604a01630ec44605a4c904937b
Bwei <1056068429@qq.com>
2024-12-15 19:11:45 +0800

    改了一点bug

---
+++ OrderCommit/src/test/java/com/example/ordercommit/controller/OrderControllerTest.java
@@ -0,0 +1,25 @@
+package com.example.ordercommit.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderControllerTest {
+
+    @Test
+    void addOrder() {
+
+    }
+
+    @Test
+    void getAllOrders() {
+    }
+
+    @Test
+    void getMyOrders() {
+    }
+
+    @Test
+    void download() {
+    }
+}
